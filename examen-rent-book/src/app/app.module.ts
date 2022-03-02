4a61b9f8e76190699edfd0622d38c47411ff9e84
facucalbo <facuucalbo@gmail.com>
2022-03-02 20:18:38 -0300

    basic app structure done

--- examen-rent-book/src/app/app.module.ts
+++ examen-rent-book/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BookComponent } from './pages/book/book.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { BookGridComponent } from './components/book-grid/book-grid.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BookComponent,
+    AuthComponent,
+    BookGridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
