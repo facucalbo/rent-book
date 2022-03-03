aed7bc7f6e063989251499f7aa6f0f649e46d135
facucalbo <facuucalbo@gmail.com>
2022-03-03 10:38:41 -0300

    files added, these was not added by an error

---
+++ examen-rent-book/src/app/components/book-grid/book-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-grid',
+  templateUrl: './book-grid.component.html',
+  styleUrls: ['./book-grid.component.css']
+})
+export class BookGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
